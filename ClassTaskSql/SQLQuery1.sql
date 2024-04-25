6f927f6bdb52e4c87b71a82d63fb984e2b9f2f08
NijatMajidov <161591072+NijatMajidov@users.noreply.github.com>
2024-04-26 01:34:25 +0400

    Task completed

--- ClassTaskSql/SQLQuery1.sql
+++ ClassTaskSql/SQLQuery1.sql
@@ -5,15 +5,11 @@ create table Categories (
 Id int primary key identity,
 Name nvarchar(20) unique not null 
 )
-insert into Categories VALUES ('CAR'),('flower')
-insert into Categories VALUES ('Russian')
+
 create table Tags (
 Id int primary key identity,
 Name nvarchar(20) unique not null 
 )
-
-insert into Tags VALUES ('#CAR'),('#flower')
-
 
 create table Users (
 Id int primary key identity,
@@ -22,19 +18,12 @@ FullName nvarchar(20) not null,
 Age int check(Age>0 and Age<150) 
 )
 
-insert into users values ('user1','User admin',18)
-insert into users values ('user2','User2 admin',39)
-insert into users values ('sari','sari veli',33)
-
 create table Blogs (
 Id int primary key identity,
 UserId int references Users(Id),
 CategoryId int references Categories(Id),
 Title nvarchar(50) check(LEN(Title)>0 and Len(Title)<50) not null
 )
-insert into Blogs values (1,1,'bla bla')
-insert into Blogs values (3,3,'Rusiya')
-insert into Blogs values (2,2,'duck')
 
 create table Comments(
 Id int primary key identity,
@@ -42,57 +31,123 @@ UserId int references Users(Id),
 BlogId int references Blogs(Id),
 Comment nvarchar(250) check(LEN(Comment)>0 and Len(comment)<250) not null
 )
-insert into Comments values (1,1,'bla bla')
-insert into Comments values (3,3,'?????')
-insert into Comments values (2,2,'VAK')
 
 Create table BlogTag(
 Id int primary key identity,
 TagId int references Tags(Id),
 BlogId int references Blogs(Id),
 )
-INSERT INTO BlogTag VALUES 
+
+--Insertler 
+insert into Categories 
+values 
+('CAR'),
+('flower'),
+('Russian')
+
+insert into Tags
+values 
+('#CAR'),
+('#flower'),
+('#tag')
+
+insert into Users 
+values 
+('user1','User admin',18),
+('user2','User2 admin',39),
+('sari','sari veli',33)
+
+insert into Blogs
+values 
+(1,1,'bla bla'),
+(2,2,'duck'),
+(3,3,'Rusiya')
+
+insert into Comments
+values
+(1,1,'bla bla'),
+(2,2,'lorem'),
+(3,3,'impus')
+
+INSERT INTO BlogTag 
+VALUES 
 (1,1),
 (3,2),
 (2,3)
 
-create view ups_TitleUserAndFullName
+--viewlar
+create view TitleUserAndFullName
 as
-select b.Title, u.UserName , u.FullName from Blogs as b
-join Users as u on b.UserId = u.Id
+select b.Title, u.UserName , u.FullName 
+from Blogs as b
+join Users as u 
+on 
+b.UserId = u.Id
 
-select * from ups_TitleUserAndFullName
+select * from TitleUserAndFullName
 
-create view ups_TitleCategoryName
+create view TitleCategoryName
 as
-select b.Title, c.Name from Blogs as b
-join Categories as c on b.CategoryId = c.Id
+select b.Title, c.Name
+from Blogs as b
+join Categories as c 
+on
+b.CategoryId = c.Id
 
-select * from ups_TitleCategoryName
+select * from TitleCategoryName
 
-create procedure ups_UserComments @userId int 
+--prosedurlar
+create procedure ups_GetUserComments @userId int 
 as
-select * from Comments as c
-where c.UserId = @userId
+select * from Comments
+where UserId = @userId
 
-execute dbo.ups_UserComments 1
+execute dbo.ups_GetUserComments 1
 
-create procedure ups_UserBlogs @userId int 
+create procedure ups_GetUserBlogs @userId int 
 as
-select * from blogs as b
-where b.UserId = @userId
+select * from blogs
+where UserId = @userId
 
-execute dbo.ups_UserBlogs 2
+execute dbo.ups_GetUserBlogs 2
 
-
+--function
 create function countBlog (@catogoryId INT)
 RETURNS	INT	
 AS
 begin
-DECLARE @Count INT 
-SELECT @Count =  COUNT(*) from Blogs as b
+declare @Count INT 
+select @Count =  COUNT(*) 
+from Blogs as b
 where b.CategoryId = @catogoryId
 return @Count
 end
 
-SELECT  DBO.countBlog (1)
+select  dbo.countBlog (1) as [Count]
+
+create function GetUserBlogs (@UserId INT)
+RETURNS	table	
+AS
+return(
+select * from Blogs
+    WHERE UserId = @userId
+)
+SELECT * FROM dbo.GetUserBlogs(3);
+--trigger
+ALTER TABLE Blogs
+ADD isDeleted BIT DEFAULT 0;
+
+CREATE TRIGGER TR_Blogs_Delete
+ON Blogs
+INSTEAD OF DELETE
+AS
+BEGIN
+    UPDATE b
+    SET b.isDelete = 1
+    FROM deleted d
+    INNER JOIN Blogs b ON d.Id = b.Id;
+END;
+
+DELETE FROM Blogs WHERE Id = 3;
+SELECT * FROM deleted;
+SELECT * FROM Blogs;
